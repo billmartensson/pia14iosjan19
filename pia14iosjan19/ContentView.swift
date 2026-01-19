1e20595335ad82c84e992eb815dd2e4ed9d66348
Bill Mårtensson <bill@magictechnology.se>
2026-01-19 11:30:24 +0100

    pia14

--- pia14iosjan19/ContentView.swift
+++ pia14iosjan19/ContentView.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 import CoreLocation
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
     
     @State var loccode = LocationCode()
+        
+    @State var mappos = MapCameraPosition.region(
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), latitudinalMeters: 5000, longitudinalMeters: 5000)
+    )
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Map(position: $mappos) {
+                ForEach(locations) { location in
+                    Marker(location.name, coordinate: location.coordinate)
+                }
+            }
+            .mapStyle(.imagery)
+            .onMapCameraChange { context in
+                print("MAP MOVED")
+                print(context.region.center.latitude)
+            }
+            
             
             Button("Get location") {
                 loccode.getposition()
--- pia14iosjan19/LocationCode.swift
+++ pia14iosjan19/LocationCode.swift
@@ -21,19 +21,45 @@ import CoreLocation
     
     func getposition() {
         
+        if manager.authorizationStatus == .notDetermined {
+            print("requestWhenInUseAuthorization")
+            manager.requestWhenInUseAuthorization()
+        }
         
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            print("requestLocation")
+            //manager.startUpdatingLocation()
+            manager.requestLocation()
+        }
         
-        manager.requestLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(locations)
         location = locations.first?.coordinate
+
+        /*
+        if locations.first!.horizontalAccuracy < 10.0 {
+            location = locations.first?.coordinate
+            manager.stopUpdatingLocation()
+        }
+         */
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
         print("LOCATION ERROR")
         print(error.localizedDescription)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            manager.requestLocation()
+        }
+        
+        if manager.authorizationStatus == .denied {
+            print("POS DENIED")
+        }
+        
     }
     
 }
